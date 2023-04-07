01da611d2c3b9b7e874aae029eb5fc961f2342a7
Ietu <54209182+Ietu@users.noreply.github.com>
2023-04-07 11:58:48 +0300

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -49,7 +49,7 @@ rows = [g * base + r for g in shuffle(rBase) for r in shuffle(rBase)]
 cols = [g * base + c for g in shuffle(rBase) for c in shuffle(rBase)]
 nums = shuffle(range(1, base * base + 1))
 
-# produce board using randomized baseline pattern
+#produce board using randomized baseline pattern
 board = [[nums[pattern(r, c)] for c in cols] for r in rows]
 boardCopy = [[nums[pattern(r, c)] for c in cols] for r in rows]
     
